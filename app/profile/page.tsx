eb91e15d4721f5395cc274919142b7541472fdb9
Kevin Sun <krsun05@gmail.com>
2024-05-18 22:02:37 -0700

    Significant styling change of Profile Page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -16,25 +16,36 @@ const Profile = (props: UserProfileProps) => {
   return (
     <>
       <Navbar />
-      <div className= {styles.profileContainer}>
-        <Image src = {props.profilePictureUrl} alt = "Profile Picture" className={styles.profilePicture} width = {300} height = {400}/>
-        <h1 className= {styles.profileHeader}>{props.name}</h1>
-        <div className = {styles.profileInfo}>
-          <p>@{props.username}</p>
-          <p>UCLA {props.grad_date}</p>
-          <p>Email: {props.email}</p>
-          <p>Ride Share Count: {props.numberOfShares}</p>
+      <div className={styles.profileContainer}>
+        <div className={styles.profileInfoContainer}>
+          <Image src={props.profilePictureUrl} alt="Profile Picture" className={styles.profilePicture} width={150} height={150} />
+          <div className={styles.profileInfo}>
+            <h1>{props.name}</h1>
+            <h3>@{props.username}</h3>
+            <p>UCLA {props.grad_date}</p>
+            <p>Ride Share Count: {props.numberOfShares}</p>
+            <div className={styles.buttonContainer}>
+              <button className={styles.buttonWithImage}>
+                <Image src="/helpSymbol.webp" alt="Help Icon" className={styles.buttonIcon} width={30} height={30}/>
+                Help
+              </button>
+              <button className={styles.buttonWithImage}>
+                <Image src="/receipt.png" alt="Activity Icon" className={styles.buttonIcon} width={25} height={25}/>
+                Activity
+              </button>
+            </div>
+          </div>
         </div>
+        <button className={`${styles.button} ${styles.editButton}`}>Edit Profile</button>
       </div>
     </>
-  )
-
+  );
 }
 
 const Page = () => {
   const userData: UserProfileProps = {
     name: 'Kevin Sun',
-    username: 'wheelsonduck',
+    username: 'duckonwheels',
     grad_date: '2027',
     email: 'krsun05@g.ucla.edu',
     numberOfShares: 42,
