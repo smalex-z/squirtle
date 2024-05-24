2ae4138434cc7eebcded0b8b7efff86785dda2af
Nathan Zhang <nml707805@gmail.com>
2024-05-23 17:26:18 -0700

    Revert back to complete and working frontend version without additional sql files, ready for PR for backend integration.

--- public/sql/rides.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE rides (
-    ride_id INT AUTO_INCREMENT PRIMARY KEY,
-    driver_id INT,
-    location VARCHAR(255),
-    destination VARCHAR(255),
-    departure_time DATETIME,
-    max_capacity INT
-    price INT
-);
