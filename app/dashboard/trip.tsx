f5be01c5b2f239ca0858f51ae2a110204d82835b
Kevin Sun <krsun05@gmail.com>
2024-06-01 19:11:43 -0700

    general outline of dashboard page complete

--- app/dashboard/trip.tsx
+++ app/dashboard/trip.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './trip.css';
+import Image from 'next/image';
 
 interface TripProps {
     date: string;
@@ -11,7 +12,7 @@ interface TripProps {
 const Trip: React.FC<TripProps> = ({ date, time, fare, address }) => {
   return (
     <div className="trip">
-      <img src="../logoSquirtle.png" alt="Car" className="trip-icon" />
+      <Image src="/logoSquirtle.png" alt="Car" className="trip-icon" width={40} height= {40}/>
       <div className="trip-details">
         <div className="trip-address">{address}</div>
         <div className="trip-datetime">{date} • {time}</div>
