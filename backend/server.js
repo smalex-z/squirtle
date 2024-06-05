a0e8ef293e8cb13308ea33073b15eaf83ac6713d
razorback4417 <razorback4417.800@gmail.com>
2024-06-04 17:14:32 -0700

    backend for registration and login working

--- backend/server.js
+++ backend/server.js
@@ -2,36 +2,34 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const tripRoutes = require('./routes/trips')
 const cors = require('cors')
+const tripRoutes = require('./routes/trips')
+const authRoutes = require('./routes/auth')
 
 // express app
 const app = express()
 
 // middleware
-app.use(cors({origin:"http://localhost:3000"}))
-
+app.use(cors({ origin: "http://localhost:3000" }))
 app.use(express.json())
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
+  console.log(`${req.method} ${req.path}`)
   next()
 })
 
 // routes
 app.use('/api/trips', tripRoutes)
+app.use('/api/auth', authRoutes)
 
-
-//connect to database
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+// connect to database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => { // process.env.PORT is the port number from the .ENV file. 
-        console.log('connected to db & listening on port', process.env.PORT)
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
     })
-})
-.catch((err) => {
-  console.log(err)
-})
-
-
+  })
+  .catch((err) => {
+    console.error(err)
+  })
